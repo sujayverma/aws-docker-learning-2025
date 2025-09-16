b291c4bbc3d1abe0a9e4ad1c709e65ebbd2a2863
Sujay Barma <sujayverma124@gmail.com>
2025-09-16 11:19:30 +0000

    Add DB seed and other bash files

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,1 +1,22 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS public.users (
+    uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    display_name text,
+    handle text,
+    email text,
+    cognito_user_id text,
+    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.activities (
+    uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_uuid UUID NOT NULL,
+    message text NOT NULL,
+    replies_count integer DEFAULT 0,
+    reposts_count integer DEFAULT 0,
+    likes_count integer DEFAULT 0,
+    reply_to_activity_uuid integer,
+    expires_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
+);
---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,21 @@
+ -- this file was manually created
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
+VALUES
+  ('The Devil', 'papamfall' ,'papemfall@gmail.com','MOCK'),
+  ('Sokhna Ngom', 'sokhnangom', 'sokhnangom2001@gmail.com' ,'MOCK');
+
+
+ -- ('pmf', 'pmf' ,'papemfall@gmail.com','MOCK');
+
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'papamfall' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'sokhnangom' LIMIT 1),
+    'I am the other!',
+    current_timestamp + interval '10 day'
+  );
