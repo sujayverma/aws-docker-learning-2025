d13c1cf1d9a2c3809f05c259492d96dd02dc08e9
Sujay Barma <sujayverma124@gmail.com>
2025-10-07 13:53:46 +0000

    Connected local db postgres

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -51,31 +51,31 @@ class HomeActivities:
         'replies': []
       }]
       sql = query_wrap_array("""
-        SELECT
-          activities.uuid,
-          users.display_name,
-          users.handle,
-          activities.message,
-          activities.replies_count,
-          activities.reposts_count,
-          activities.likes_count,
-          activities.reply_to_activity_uuid,
-          activities.expires_at,
-          activities.created_at
-        FROM public.activities
-        LEFT JOIN public.users ON users.uuid = activities.user_uuid
-        ORDER BY activities.created_at DESC
+       SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
       """)
       print(sql)
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           json = cur.fetchone()
-      print('HHHHHHHHHHHHHHH')
-      print(json)
-      print(json[0])
-      span.set_attribute("app.result_count", len(results))
-      return results
+      # print('HHHHHHHHHHHHHHH')
+      # print(json)
+      # print(json[0])
+      span.set_attribute("app.result_count", len(json[0]))
+      return json[0]
 
 # class HomeActivities:
 #   def run():
